509599ee40f7417bfe6f425b41ed2fe9ad8c92f4
David <davidstankoviic@gmail.com>
2021-05-18 21:08:03 +0200

    Add: Facebook Authentication, User Profile Photo and Name

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 module.exports = {
     images: {
         domains: [
-            "firebasestorage.googleapis.com"
+            "firebasestorage.googleapis.com",
+            "platform-lookaside.fbsbx.com",
+            "scontent.fbeg4-1.fna.fbcdn.net",
+            "http://graph.facebook.com"
         ]
     }
 }
