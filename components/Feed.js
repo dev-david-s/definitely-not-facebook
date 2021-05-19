b213125459b785a35bf27c37c9a27d3eae3aeba7
David <davidstankoviic@gmail.com>
2021-05-19 20:43:09 +0200

    Add: Post Component, Initial structure for widget menu

--- components/Feed.js
+++ components/Feed.js
@@ -1,13 +1,14 @@
 import InputBox from "./InputBox"
+import Posts from "./Posts"
 import Stories from "./Stories"
 
 function Feed() {
     return (
-        <div className='flex-grow h-screen pb-44 pt-6 md:mr-4 xl:mr-40 overflow-y-auto'>
+        <div className='flex-grow h-screen pb-44 pt-6 md:mr-4 xl:mr-40 overflow-y-auto no-scrollbar'>
             <div className="mx-auto max-w-md md:max-w-lg lg:max-w-2xl">
                 <Stories />
                 <InputBox />
-                {/**POSTS HERE */}
+                <Posts />
             </div>
         </div>
     )
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Feed from '../components/Feed';
 import Header from '../components/Header'
 import Login from '../components/Login';
 import Sidebar from '../components/Sidebar';
+import Widgets from '../components/Widgets';
 
 export default function Home({ session }) {
   if (!session) return <Login />;
@@ -18,7 +19,7 @@ export default function Home({ session }) {
 
         <Sidebar />
         <Feed />
-        {/** Widgets */}
+        <Widgets />
       </main>
     </div>
   )
